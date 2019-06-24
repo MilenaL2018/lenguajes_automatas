0ace8049126e7407a4dad014a11d38ac3bc72421
Agustin <agustinmaurasvelez@gmail.com>
2019-06-24 11:33:02 -0300

    MT

--- automatas.js
+++ automatas.js
@@ -16,6 +16,7 @@ window.onload = function() {
 
             reader.onload = function(e) {
                 fileDisplayArea.innerText = reader.result;
+                alert(reader.result);
                 mijson = JSON.parse(reader.result);
 
             };
@@ -222,7 +223,7 @@ window.onload = function() {
 
                         if (mijson.transiciones[q].apilo == "l"){
                             pila.pop();
-                            
+
                             if (pila.length == 1){
                                 if (i === chain.length - 1){
                                     alert(chain.length - 1 + " " + i);
@@ -255,12 +256,135 @@ window.onload = function() {
                     return;
                 }
             }
+        } else if (mijson.automata === "MT"){
+            //Verifico que todos los caracteres de la cinta esten en el alfabeto
+            for (i = 0; i < mijson.cinta.length; i++){
+                for (q = 0; q < mijson.alfabetoDeCinta.length; q++){
+                    if (mijson.cinta[i] == mijson.alfabetoDeCinta[q]){
+                        break;
+                    }
+                    if (mijson.cinta[i] !== mijson.alfabetoDeCinta[q] && mijson.alfabetoDeCinta[q] === mijson.alfabetoDeCinta[mijson.alfabetoDeCinta.length - 1] ){
+                        alert("Caracter incorrecto: " + mijson.cinta[i]);
+                        return;
+                    }
+                }
+            }
+
+            //Verifico que el estado inicial sea usado
+            for (i = 0; i < mijson.transiciones.length; i++){
+                if (mijson.transiciones[i].actual == mijson.estadoInicial ){
+                    break;
+                }
+                if (mijson.transiciones[i].actual !== mijson.estadoInicial && mijson.transiciones[i] === mijson.transiciones[mijson.transiciones.length - 1]){
+                    alert("Estado inicial sin transicion definida");
+                    return;
+                }
+            }
+
+            let transicionOk = false;
+
+            for (i = 0; i < mijson.transiciones.length; i++){
+                //Verifico todos los estados
+                for (q = 0; q < mijson.estados.length; q++){
+                    if (mijson.transiciones[i].actual == mijson.estados[q]){
+                        for (p = 0; p < mijson.estados.length; p++){
+                            if (mijson.transiciones[i].proximo == mijson.estados[p]){
+                                transicionOk = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (transicionOk === true){
+                        transicionOk = false;
+                        break;
+                    }
+
+                    if (mijson.transiciones[i].actual !== mijson.estados[q] && mijson.transiciones[i].proximo !== mijson.estados[q] && mijson.estados[q] === mijson.estados[mijson.estados.length - 1]){
+                        alert("Error de estados en transiciones");
+                        return;
+                    }
+                }
+            }
+
+            for (i = 0; i < mijson.transiciones.length; i++){
+                //Verifico el alfabeto
+                for (w = 0; w < mijson.alfaalfabetoDeCintabeto.length; w++){
+                    if(mijson.transiciones[i].valor == mijson.alfabetoDeCinta[w]){
+                        break;
+                    }
+                    if (mijson.transiciones[i].valor !== mijson.alfabetoDeCinta[w] && mijson.alfabetoDeCinta[w] === mijson.alfabetoDeCinta[mijson.alfabetoDeCinta.length - 1] && mijson.transiciones[i] === mijson.transiciones[mijson.transiciones.length - 1]){
+                        alert("Error de alfabeto en transiciones");
+                        return;
+                    }
+                }
+            }
+
+            //Verifico los estados de salida
+            for (i = 0; i < mijson.transiciones.length; i++){
+                if (mijson.transiciones[i].proximo == mijson.estadoSalida){
+                    break;
+                }
+
+                if ( mijson.transiciones[i].proximo !== mijson.estadoSalida && mijson.transiciones[i] === mijson.transiciones[mijson.transiciones.length - 1]){
+                    alert("Error en los estados de salida");
+                    return;
+                }
+            }
+
+
+            //Recorro el automata
+
+            let actualState = mijson.estadoInicial;
+            let cinta = mijson.cinta;
+            let index = 0;
+            let error = false;
+
+            for (q = 0; q < mijson.transiciones.length; q++){
+
+                if (mijson.transiciones[q].actual == actualState && mijson.transiciones[q].valor == cinta[index]){
+
+                    actualState = mijson.transiciones[q].proximo;
+                    cinta[index] = mijson.transiciones[q].escribo;
+
+                    if(mijson.transiciones[q].direccion == "I"){
+                        if(index > 0){
+                            index--;
+                        } else {
+                            index = 0;
+                        }
+                    } else if (mijson.transiciones[q].direccion == "D"){
+                        if(index < cinta.length - 1){
+                            index++;
+                        } else {
+                            index++;
+                            cinta.push("b");
+                        }
+                    } else {
+                        alert("Error en definicion de direccion");
+                        error = true;
+                    }
+                    break;
+                }
+                if (error){
+                    break;
+                }
+            }
+
+            if(!error){
+                //Verifico si da salida si no hubo un error antes
+                for (i = 0; i < mijson.estadosSalida.length; i++){
+                    if( actualState == mijson.estadosSalida[i]){
+                        alert("Cadena Correcta");
+                        return;
+                    }
+                    if(actualState !== mijson.estadosSalida[i] && mijson.estadosSalida[i] === mijson.estadosSalida[mijson.estadosSalida.length - 1]){
+                        alert("Cadena Incorrecta");
+                        return;
+                    }
+                }
+            }
         }
-
-
-
-
-
     });
 };
 
